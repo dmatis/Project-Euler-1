74ee869279f3e8a34e417719cdd5771eeae07b31
amilner42 <amilner42@gmail.com>
2015-02-05 20:40:51 -0800

    Problem 1 tiny fix and Problem 2 clean

--- ProjectEuler/src/problems1to9/Problem1.java
+++ ProjectEuler/src/problems1to9/Problem1.java
@@ -10,12 +10,14 @@ public class Problem1 {
 
 	public static void main(String[] args) {
 		int sum = 0; 
+		
 		// Go through 1-1000 and add the number to sum if it  
 		// is divisible by 3 or 5 
 		for(int i = 0; i < 1000;i++){
 		if(i % 3 == 0 || i % 5 == 0)	
 			sum += i; 
 		}
+		
 		// Print out the answer 
 		System.out.println(sum);
 	}
