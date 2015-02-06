bc060cbfb415f03a5a85f476e846920a2a70cc92
amilner42 <amilner42@gmail.com>
2015-02-05 21:31:42 -0800

    Cleaned Problem 3

--- ProjectEuler/src/problems1to9/Problem3.java
+++ ProjectEuler/src/problems1to9/Problem3.java
@@ -1,30 +1,36 @@
 package problems1to9;
 
+import personal_library.MathFunctions;
+
+//Question -----------------------------------------------------------------------------------------------------------------------
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
+//---------- -----------------------------------------------------------------------------------------------------------------------
+// Solution correct  
 
-//correct 
 public class Problem3 {
 
 	
 	public static void main(String[] args) {
-		//the number we will be finding the prime factor of 
+		// the number we will be finding the prime factor of 
 		long num1 = 600851475143L;
 		long num2 = 1; 
 		long num3 = 2; 		
 		
-		//clean loop-control variable 
+		// clean loop-control variable 
 		boolean breaknow = false; 
 		
 		// Go through all the numbers, check if the number is a factor and then check if 
 		// it is a prime. Because we are dividing from smallest to biggest the first prime
-		// factor will be the largest 
+		// factor will be the largest guaranteed
 		while(!(num2 == num1)&& !breaknow){
 			num3 =num1/num2; 
 			
 			// check the number is a factor (no rounding error) and then check
-			// if the number is a prime
-			if(num1 == (num2 * num3) && checkPrime(num3)){
+			// if the number is a prime number after. (This order more efficient
+			// then checking prime first). (Java && short-circuit)
+			if(num1 == (num2 * num3) && MathFunctions.checkPrime(num3)){
+				// print out the answer 
 				System.out.println(num3);
 				breaknow = true; // break loop 
 			}
@@ -33,21 +39,5 @@ public class Problem3 {
 			}
 		}
 	}
-	
-		
-
-	
-	
-	//checks if given integer is prime (1 and 2 are prime)
-		private static boolean checkPrime(long primeNumber) {
-			int a = 2; 
-			while(a <= Math.sqrt(primeNumber)){
-				if(primeNumber % a == 0){
-					return false;
-				}
-				a++; 
-			}
-	     return true; 
-		}
 
 }
