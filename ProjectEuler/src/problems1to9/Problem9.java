6da8234693ebe96d319cd7b7fd31968e49a60774
amilner42 <amilner42@gmail.com>
2015-02-07 17:42:48 -0800

    Cleaned and recoded Problem 9

--- ProjectEuler/src/problems1to9/Problem9.java
+++ ProjectEuler/src/problems1to9/Problem9.java
@@ -1,34 +1,38 @@
 package problems1to9;
 
-//correct 
-//relativly easy, just use two for loops and be smart with what you go up to
-import java.math.BigInteger;
-
+//Question -----------------------------------------------------------------------------------------------------------------------
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+// a^2 + b^2 = c^2
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+//---------- -----------------------------------------------------------------------------------------------------------------------
+//Solution correct  
 
 public class Problem9 {
 
 	public static void main(String[] args) {
-		
-		for(int a = 0; a <= 500; a++){
-			BigInteger aBig = BigInteger.valueOf(a);
-	
 			
-			for(int b = 0; b <= 500; b++){
-				BigInteger bBig = BigInteger.valueOf(b);
-				
-				double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-				if(c == (int)c && aBig.add(bBig.add(BigInteger.valueOf((long) c))).equals(BigInteger.valueOf(1000))){
-					//System.out.println(Double.toString((a * (double)b *(double)c)));
-					System.out.println(aBig.multiply(bBig.multiply(BigInteger.valueOf((long) c))));
+		// Loop through all the combinations for variables a and b, where at most both are 499
+		// I picked both limits as 499 as to make sure to encapsulate all the combinations that 
+		// could be the answer, this set of possibilities is definitely a super-set of the 
+		// possibilities I needed to search. (for ease of programming) 
+		outerLoop:
+		for(int a = 0; a < 500; a++){
+			
+			for(int b = a; b < 500; b++){
+				// Check to see if it has a clean sqaure root, and check if a + b + c = 1000
+				long cSquared = a*a + b*b;
+				long c = (long) Math.sqrt(cSquared);
+				if(c * c == cSquared){
+					if(a + b + c == 1000){
+						// print out the answer 
+						System.out.println(a*b*c);
+						break outerLoop; 
+					}
 				}
 				
-				
-				
-				
-				
 			}
-			
-			
 			
 		}
 	
@@ -36,3 +40,4 @@ public class Problem9 {
 	}
 
 }
+
