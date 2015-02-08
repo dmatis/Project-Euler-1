8edd0caf9ff3bc4d5921b2141e32b9c9486f536d
amilner42 <amilner42@gmail.com>
2015-02-07 22:54:11 -0800

    Cleaned Problem 16

--- ProjectEuler/src/problems10to19/Problem16.java
+++ ProjectEuler/src/problems10to19/Problem16.java
@@ -1,29 +1,28 @@
 package problems10to19; 
 
-//correct 
-//easy use BigInteger
 import java.math.BigInteger;
 
+//Question -----------------------------------------------------------------------------------------------------------------------
+//  2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//  What is the sum of the digits of the number 2^1000?
+//---------- -----------------------------------------------------------------------------------------------------------------------
+// Solution correct 
 
 public class Problem16 {
 
 	public static void main(String[]args){
+		// Using BigInteger get 2^1000 and convert it to a char array
+		long sum = 0; 
+		BigInteger number = (new BigInteger("2")).pow(1000);
+		char [] fullNumber = number.toString().toCharArray();
 		
-		BigInteger number = BigInteger.valueOf(2);
-		BigInteger sum    = BigInteger.valueOf(0);	
-		
-		
-		for(int i = 1; i < 1000; i++){
-			number = number.multiply(BigInteger.valueOf(2));
-			
+		// Go through this array adding every value, subtracting 48 to deal
+		// with the conversion from char '1' to integer 1. eg. '1' = 49
+		for(int i = 0; i < fullNumber.length; i++){
+			sum += fullNumber[i] - 48; 
 		}
 
-        while(!number.equals(BigInteger.ZERO)){
-        	BigInteger tempHolder = number.remainder(BigInteger.TEN);
-        	sum = sum.add(tempHolder);
-        	number = number.divide(BigInteger.TEN);
-        }
-        
+		// Print out the answer 
         System.out.println(sum);
 		
 	}
