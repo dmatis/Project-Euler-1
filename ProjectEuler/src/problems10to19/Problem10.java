75a21a100502f1aa0e6b98506eb07c66a5bc4197
amilner42 <amilner42@gmail.com>
2015-02-07 17:52:50 -0800

    Cleaned Problem 10

--- ProjectEuler/src/problems10to19/Problem10.java
+++ ProjectEuler/src/problems10to19/Problem10.java
@@ -1,43 +1,27 @@
 package problems10to19; 
 
-//correct
-//contains prime method
-//careful large numbers needed!
+import personal_library.MathFunctions;
 
-import java.math.BigInteger;
-
+//Question -----------------------------------------------------------------------------------------------------------------------
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+// Find the sum of all the primes below two million.
+//---------- -----------------------------------------------------------------------------------------------------------------------
+// Solution correct 
 
 public class Problem10 {
-
+	
 	public static void main(String[] args) {
-		BigInteger sum = BigInteger.ZERO; 
+		long sum = 0;
 		
-		
-		//System.out.println(checkPrime(4));
-		//System.out.println(checkPrime(81));
-		//System.out.println(checkPrime(101));
-		for(int i = 2; i < 2*1E6; i++){
-			if(checkPrime(i)){
-				//System.out.println(i);
-				sum = sum.add(BigInteger.valueOf(i));
+		// Go through all the numbers less than 2 million
+		for(int i = 0; i < 2*1E6; i++){
+			if(MathFunctions.checkPrime(i)){
+				sum += i; 
 			}
-			
 		}
 		
+		// Print out the answer 
 		System.out.println(sum);
 
 	}
-	
-	//checks if given integer is prime 
-	private static boolean checkPrime(int primeNumber) {
-		int a = 2; 
-		while(a <= Math.sqrt(primeNumber)){
-			if(primeNumber % a == 0){
-				return false;
-			}
-			a++; 
-		}
-     return true; 
-	}
-
 }
