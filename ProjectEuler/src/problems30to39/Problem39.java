57c7cc2bb82d0d98a052da152aa0f894626f17fd
Arie Milner <amilner42@gmail.com>
2015-03-15 15:32:43 -0700

    Spellcheck failure...Problem 39

--- ProjectEuler/src/problems30to39/Problem39.java
+++ ProjectEuler/src/problems30to39/Problem39.java
@@ -14,17 +14,17 @@ package problems30to39;
 public class Problem39 {
 
 	public static void main(String[] args) {
-		int[] paramPossib= new int[1001];					// Keep track of the number of possibilities for each parameter  paramPossib[some-param] = number of possibilities
+		int[] paramPossib= new int[1001];					// Keep track of the number of possibilities for each perimeter  paramPossib[some-param] = number of possibilities
 
-		// Go through all the possibilities, no need to check for side-lengths over 500 because then the parameter will be over 1000. 
-		// Every time you find a proper right triangle then add 1 point to that parameter (if the parameter is <= 1000) 
+		// Go through all the possibilities, no need to check for side-lengths over 500 because then the perimeter will be over 1000. 
+		// Every time you find a proper right triangle then add 1 point to that perimeter (if the perimeter is <= 1000) 
 		for(int a=0; a<=500; a++) {
 			for(int b=a; b<=500; b++) {
 				double cDouble = Math.sqrt(a*a+b*b);		// get the length of the hypotenuse (precise)
 				int c = (int) cDouble;						// get the length of the hypotenuse (rounded)
 				
 				// If the hypotenuse is an exact integer (eg 5 or 8, not 12.2 or 3.333) &&
-				// the total parameter of the triangle is <= 1000 then add 1 point to that parameter
+				// the total perimeter of the triangle is <= 1000 then add 1 point to that perimeter
 				if(c == cDouble) {
 					if(a+b+c <= 1000) {
 						paramPossib[a+b+c]++;
@@ -33,8 +33,8 @@ public class Problem39 {
 			}
 		}
 
-		int most = 0;										// the number of solutions for the parameter with the most solutions 
-		int index = 0;										// the parameter with the most solutions 
+		int most = 0;										// the number of solutions for the perimeter with the most solutions 
+		int index = 0;										// the perimeter with the most solutions 
 		for(int i=0; i<=1000; i++) {
 			if(paramPossib[i] > most) {
 				most=paramPossib[i];
