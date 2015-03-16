4165f1fe08287ffa4ef30d5ba31c8b9c223c5b38
Arie Milner <amilner42@gmail.com>
2015-03-15 18:22:32 -0700

    Cleaned Problem 40

--- ProjectEuler/src/problems40to49/Problem40.java
+++ ProjectEuler/src/problems40to49/Problem40.java
@@ -1,26 +1,37 @@
 package problems40to49;
 
-//correct
-//super easy the way I did it
-//run time -57-59 seconds 
+//Question -----------------------------------------------------------------------------------------------------------------------
+//	An irrational decimal fraction is created by concatenating the positive integers:
+//	
+//	0.123456789101112131415161718192021...
+//	
+//	It can be seen that the 12th digit of the fractional part is 1.
+//	
+//	If dn represents the nth digit of the fractional part, find the value of the following expression.
+//	
+//	d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+//---------- -----------------------------------------------------------------------------------------------------------------------
+// Solution correct
+// Runtime long: 48 seconds 
+
 public class Problem40 {
 
 	public static void main(String[] args) {
-		String numberConcat = "1";
-		int index = 2; 
-		while(numberConcat.length()<1000100){
-		//System.out.println(index);
-		String tempString = String.valueOf(index);
-		numberConcat = numberConcat.concat(tempString);
-		index++; 
+		String numberConcat = "";										// going to record all the numbers concatenated
+		
+		// Go through the integers concatenating them to numberConcat. 
+		for(int index = 1; numberConcat.length() <= 1000000; index++) {
+			numberConcat = numberConcat.concat(String.valueOf(index));
 		}
-		int mult = 1; 
-		int total = 1; 
-		while(mult <= 1E6){
-		total *= Integer.valueOf(String.valueOf(numberConcat.charAt(mult-1)));
-		mult *= 10; 
-		}
-		System.out.println(total);
-	}
 
+		int nthDigit = 1;												// the nth digit 
+		int currentProduct = 1; 										// the current product of multiplying the dn
+		// Multiply the dn 
+		while(nthDigit <= 1E6) {
+			currentProduct *= Integer.valueOf(String.valueOf(numberConcat.charAt(nthDigit-1)));
+			nthDigit *= 10; 
+		}
+		// print out the answer
+		System.out.println(currentProduct);
+	}
 }
